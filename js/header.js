66e3a09c5c7f2603cba3d57046cda6964fff003b
EdgarLee0816 <ryunoske84@gmail.com>
2024-08-08 11:48:06 +0900

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const navSlide = () => {
         //nav.classList.add("slide")
 
-        iframe.classList.add("slide")
+        iframe.classList.add("slide");
         
 
     };
