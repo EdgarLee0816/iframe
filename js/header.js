9ca4cfe8cb251bc68529430fa9d3bad9f6c3503e
EdgarLee0816 <ryunoske84@gmail.com>
2024-08-08 11:21:48 +0900

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const navBtn = document.querySelector("header button");
+    const nav = document.querySelector("header nav");
+    const iframe = window.parent.document.getElementById("header");
+
+
+    const navSlide = () => {
+        //nav.classList.add("slide")
+
+        iframe.classList.add("slide")
+        
+
+    };
+    
+    navBtn.addEventListener("click", navSlide);
+
+
+});//////////////////끝
